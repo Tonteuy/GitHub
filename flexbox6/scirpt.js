9d1977006768215839e366165eae2cad99edca0d
Tonteuy <tonteuy@hotmail.com>
2024-07-07 19:57:58 +0700

    home
    my home

---
+++ flexbox6/scirpt.js
@@ -0,0 +1,13 @@
+$("#menu_button").click(function() {
+    $("#site-navigation").toggleClass("toggled");
+  });
+  
+  /* A guide for flex-box
+    https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+  */
+  
+  /*
+  Pure CSS Responsive Flex Menu
+  http://blog.christopherianmurphy.com/2016/01/responsive-pure-css-menu.html
+  */
+  
